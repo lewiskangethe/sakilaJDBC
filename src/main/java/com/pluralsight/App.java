85b59fce58b3db97260538c5349fd3b3aecc2654
lewiskangethe <lewiskkangethe@gmail.com>
2025-12-02 11:02:13 -0500

    commit with more column selections and closed database resources

--- src/main/java/com/pluralsight/App.java
+++ src/main/java/com/pluralsight/App.java
@@ -6,31 +6,43 @@ public class App {
 
     public static void main(String[] args) throws SQLException {
         String url = "jdbc:mysql://localhost:3306/sakila";
-        String username = "root";
-        String password = "yearup";
+        String username = args[0];
+        String password = args[1];
 
         //1. open a connection to the database
         Connection connection = DriverManager.getConnection(url, username, password);
 
-//        create statement
-// the statement is tied to the open connection
-   Statement statement = connection.createStatement();
-// define your query
-        String query = "SELECT city FROM city";
+
+        // define your query
+        String query = """
+                        SELECT title, description, release_year, length
+                        FROM film;
+                        """;
+
+        // create statement
+        // the statement is tied to the open connection
+        PreparedStatement statement = connection.prepareStatement(query);
 
         // 2. Execute your query
-        ResultSet results = statement.executeQuery(query);
-
-//        System.out.println(results);
+        ResultSet results = statement.executeQuery();
 
         // process the results
         while (results.next()) {
-            String city = results.getString("city");
-            System.out.println(city);
-        }
-// 3. Close the connection
-        connection.close();
+            String title = results.getString("title");
+            String description = results.getString("description");
+            int releaseYear = results.getInt("release_year");
+            int length = results.getInt("length");
 
+            System.out.println(title);
+            System.out.println(description);
+            System.out.println(releaseYear);
+            System.out.println(length);
+            System.out.println("-----------------------");
+        }
+
+        results.close();
+        statement.close();
+        connection.close();
 
     }
 }
